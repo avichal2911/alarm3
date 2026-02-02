d99b31b5959b4202fdf6840b518266091ae6206e
avichal2911 <56879351+avichal2911@users.noreply.github.com>
2026-02-02 17:03:29 +0530

    Update RingingView.swift

--- CustomAlarmApp/CustomAlarmApp/Views/RingingView.swift
+++ CustomAlarmApp/CustomAlarmApp/Views/RingingView.swift
@@ -1,36 +1,13 @@
-import SwiftUI
-
-struct RingingView: View {
-    var alarm: Alarm
-    
-    var body: some View {
-        VStack(spacing: 40) {
-            Text(timeString())
-                .font(.system(size: 60, weight: .bold))
-            
-            Text(alarm.description)
-                .font(.title2)
-            
-            HStack(spacing: 30) {
-                Button("Turn Off") {
-                    // logic added later
-                }
-                
-                Button("Mute") {
-                    // logic added later
-                }
-                
-                Button("Repeat") {
-                    // logic added later
-                }
-            }
-            .font(.title2)
-        }
+HStack(spacing: 30) {
+    Button("Turn Off") {
+        AlarmEngine.shared.turnOff()
     }
     
-    func timeString() -> String {
-        let minute = String(format: "%02d", alarm.minute)
-        return "\(alarm.hour):\(minute) \(alarm.isAM ? "AM" : "PM")"
+    Button("Mute") {
+        AlarmEngine.shared.mute()
+    }
+    
+    Button("Repeat") {
+        AlarmEngine.shared.repeatAlarm()
     }
 }
-
