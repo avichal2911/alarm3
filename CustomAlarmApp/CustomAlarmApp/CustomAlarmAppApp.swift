fd09620a17dc220fbab8a1cffc49c4d74ea9cc03
avichal2911 <56879351+avichal2911@users.noreply.github.com>
2026-02-02 16:56:42 +0530

    Update CustomAlarmAppApp.swift

--- CustomAlarmApp/CustomAlarmApp/CustomAlarmAppApp.swift
+++ CustomAlarmApp/CustomAlarmApp/CustomAlarmAppApp.swift
@@ -1,1 +1,17 @@
+import SwiftUI
+
+@main
+struct CustomAlarmAppApp: App {
+    @StateObject var alarmStore = AlarmStore()
+    
+    var body: some Scene {
+        WindowGroup {
+            AlarmListView()
+                .environmentObject(alarmStore)
+                .onAppear {
+                    NotificationManager.shared.requestPermission()
+                }
+        }
+    }
+}
 
