4bf0057c2f362097f6df3b8ab14001b23eb92304
avichal2911 <56879351+avichal2911@users.noreply.github.com>
2026-02-02 16:57:00 +0530

    Update Alarm.swift

--- CustomAlarmApp/CustomAlarmApp/Models/Alarm.swift
+++ CustomAlarmApp/CustomAlarmApp/Models/Alarm.swift
@@ -1,1 +1,28 @@
+import Foundation
+
+enum AlarmMode: String, Codable {
+    case oneTime
+    case weekly
+}
+
+struct Alarm: Identifiable, Codable {
+    var id: UUID = UUID()
+    var createdAt: Date = Date()
+    
+    var hour: Int
+    var minute: Int
+    var isAM: Bool
+    
+    var mode: AlarmMode
+    var date: Date?
+    var daysOfWeek: [Int]?
+    
+    var description: String
+    var soundName: String
+    
+    var repeatAfter: Int
+    var stopAfter: Int
+    
+    var isEnabled: Bool = true
+}
 
