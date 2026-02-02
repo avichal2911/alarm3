def8c72155651f19b3f53dad3d2f801bc40058f7
avichal2911 <56879351+avichal2911@users.noreply.github.com>
2026-02-02 17:02:03 +0530

    Update CustomAlarmAppApp.swift

--- CustomAlarmApp/CustomAlarmApp/CustomAlarmAppApp.swift
+++ CustomAlarmApp/CustomAlarmApp/CustomAlarmAppApp.swift
@@ -1,17 +1,41 @@
 import SwiftUI
+import UserNotifications
 
 @main
-struct CustomAlarmAppApp: App {
+struct CustomAlarmAppApp: App, UNUserNotificationCenterDelegate {
     @StateObject var alarmStore = AlarmStore()
+    @StateObject var engine = AlarmEngine.shared
+    
+    init() {
+        UNUserNotificationCenter.current().delegate = self
+    }
     
     var body: some Scene {
         WindowGroup {
-            AlarmListView()
-                .environmentObject(alarmStore)
-                .onAppear {
-                    NotificationManager.shared.requestPermission()
+            ZStack {
+                AlarmListView()
+                    .environmentObject(alarmStore)
+                
+                if let alarm = engine.activeAlarm {
+                    RingingView(alarm: alarm)
                 }
+            }
+            .onAppear {
+                NotificationManager.shared.requestPermission()
+            }
         }
     }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                didReceive response: UNNotificationResponse,
+                                withCompletionHandler completionHandler: @escaping () -> Void) {
+        
+        if let id = response.notification.request.content.userInfo["alarmID"] as? String,
+           let alarm = alarmStore.alarms.first(where: { $0.id.uuidString == id }) {
+            
+            AlarmEngine.shared.start(alarm: alarm)
+        }
+        
+        completionHandler()
+    }
 }
-
